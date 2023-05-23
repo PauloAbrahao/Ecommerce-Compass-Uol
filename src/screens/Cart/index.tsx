21e73c0d6c414a076d902e8f207447805f8c0999
Roberth <roberth.renner12@gmail.com>
2023-05-23 15:18:27 -0300

    feat(Screen): cartScreen initial commit

--- src/screens/Cart/index.tsx
+++ src/screens/Cart/index.tsx
@@ -1,9 +1,21 @@
 import {View, Text} from "react-native";
+import ButtonBuy from "../../components/ButtonBuy";
+import styles from "./style";
 
 const Cart = () => {
     return (
-        <View>
-            <Text>Cart</Text>
+        <View style={styles.viewPort}>
+            <View style={styles.header}>
+                <Text style={styles.text}>Total</Text>
+                <Text style={styles.textCount}>R$0</Text>
+            </View>
+            <View style={styles.container}>
+                <Text style={styles.textContainer}>Ops, Empty Cart :(</Text>
+                <Text style={styles.subTextContainer}>Add a product</Text>
+            </View>
+            <View style={styles.footerButton}>
+            <ButtonBuy children="BUY" onPress={()=>{}} isloading={false}/>
+            </View>
         </View>
     );
 };
