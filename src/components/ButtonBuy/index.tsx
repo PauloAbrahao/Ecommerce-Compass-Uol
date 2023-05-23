dcba85ccbb6de802713d129014cc1cfc94c6d958
Igor Machado <igor.oliveira.pb@compasso.com.br>
2023-05-22 21:08:35 -0300

    feat: button buy/add cart component

---
+++ src/components/ButtonBuy/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { View, Pressable, Text, ActivityIndicator } from "react-native";
+import { styles } from "./style";
+
+type ButtonBuyProps = {
+  children: string;
+  onPress: () => void;
+  isloading: boolean;
+};
+
+function ButtonBuy({
+  children,
+  onPress,
+  isloading,
+}: ButtonBuyProps): JSX.Element {
+  return (
+    <Pressable
+      style={({ pressed }) => [styles.button, pressed && styles.pressed]}
+      onPress={onPress}
+    >
+      <View>
+        {isloading ? (
+          <ActivityIndicator size={"large"} color="white" />
+        ) : (
+          <Text>{children}</Text>
+        )}
+      </View>
+    </Pressable>
+  );
+}
+
+export default ButtonBuy;
