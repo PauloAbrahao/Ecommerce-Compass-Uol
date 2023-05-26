f0b09d569daef540f42bddf256e0ff43cfcd797b
PedroHVL14 <pedro.vitorino.lima@gmail.com>
2023-05-26 15:20:57 -0300

    router-change

--- App.tsx
+++ App.tsx
@@ -1,14 +1,13 @@
 import { NavigationContainer } from "@react-navigation/native";
 import AppRoutes from "./src/config/routes/StackNavigator/index";
 import { AppProvider } from "./src/context/index";
-import ProductScreen from "./src/screens/Item_Screen/item_screen";
 
 export default function App() {
   return (
     <>
       <NavigationContainer>
         <AppProvider>
-          <ProductScreen />
+          <AppRoutes />
         </AppProvider>
       </NavigationContainer>
     </>
